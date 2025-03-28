d2a94d70b8110951bf20a99209bc44126f4f3919
David-juez <djuez@milaifontanals.org>
2025-03-28 13:05:57 +0100

    Add files via upload

---
+++ Random/src/random/Random.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package random;
+import java.math.*;
+/**
+ *
+ * @author Usuari
+ */
+public class Random {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        // TODO code application logic here
+        i=(int)(Math.random ()*100);
+        System.out.println(i);
+    }
+    
+}
